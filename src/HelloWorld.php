e0514a9cf12759230417177dd36c5d6c8494a9c1
xushasha <xushasha@banmatrip.com>
2019-12-31 15:52:18 +0800

    init-config

---
+++ src/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace pack;
+
+class HelloWorld
+{
+    public function sayHello()
+    {
+        echo 'Hello world!';
+    }
+
+    public static function sayHelloStatic()
+    {
+        echo 'Hello world static';
+    }
+}
